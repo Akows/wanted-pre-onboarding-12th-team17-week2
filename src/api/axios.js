3606afd40c46484c53722e9212f6a15031859cdb
AKOWSPC <akows141@gmail.com>
2023-08-29 20:43:34 +0900

    Fix: 엑세스 토큰을 적용하여 API를 호출하는 기능을 구현했는데, 호출 제한에 걸려 API 사용이 중단되는 문제를 발견. (엑세스 토큰을 적용했으니 제약이 거의 없어야 하는데 문제가 발생하여 토큰을 교체함.) (그런데 토큰이 정상적으로 서버에서 인증되었는지 확인하는 방법을 몰라 문제가 해결되었는지 확신을 못하는 중.)

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,5 +4,16 @@ const token = process.env.PRIVATE_ACCESS_TOKEN;
 
 export const axiosClient = axios.create({
   baseURL: `https://api.github.com/repos/`,
-  auth: token,
+  headers: {
+    Authorization: token,
+  },
+
+  // auth: token,
 });
+
+// axiosClient.interceptors.request.use(config => {
+//   if (token) {
+//     config.headers.Authorization = `token ${token}`;
+//   }
+//   return config;
+// });
